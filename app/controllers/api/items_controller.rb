23a3f94ac6c6a487c946669ce02ae5f7c24535e6
Su0414 <susanaik.com>
2018-11-30 12:54:47 -0800

    add dummy data for items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::ItemsController < ApplicationController
   def index
     @items = Item.all 
-    render json: @items
+    render json: @items , status: 200
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+items = Item.create([
+    { name: 'Sarees', price:150 }, 
+    { name: 'Punjabi Suits', price:100 },
+    { name: 'Western Suits', price:1000 },
+    { name: 'Panash Special', price:100 },
+    { name: 'Kids Suits', price:100 },
+    { name: 'Pink Punjabi Suits', price:100 },
+    { name: 'Red Punjabi Suits', price:100 },
+    { name: 'Purple Punjabi Suits', price:100 },
+    { name: 'Black Punjabi Suits', price:100 },
+    { name: 'White Punjabi Suits', price:100 }
+])
