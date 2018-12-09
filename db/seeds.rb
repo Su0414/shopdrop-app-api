71ffb7a48da5d3494751970435e4768707d790c9
Su0414 <susanaik.com>
2018-12-09 09:44:43 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,67 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 items = Item.create([
-    { name: 'Sarees', price:150 }, 
-    { name: 'Punjabi Suits', price:100 },
-    { name: 'Western Suits', price:1000 },
 
-    { name: 'Panash Special', price:100 },
-    { name: 'Kids Suits', price:100 },
-    { name: 'Pink Punjabi Suits', price:100 },
 
-    { name: 'Red Punjabi Suits', price:100 },
-    { name: 'Purple Punjabi Suits', price:100 },
-    { name: 'Black Punjabi Suits', price:100 },
-    
+    { name: ' Red Saree Women', 
+    description: 'Exclusive Womens fashion Panash Collection',
+    image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/1/2143sr15-12920.jpg',
+    price:200 },
+
+    { name: 'Mens Black Kurta', 
+      description: 'Exclusive Mens fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/m/c/mcd2924.jpg',
+      price:150 }, 
+
+      { name: 'Mens Gold Print Kurta', 
+      description: 'Exclusive Mens fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/b/r/brocade-nehru-jacket-in-rust-v1-mhg936.jpg',
+      price:60 }, 
+
+      { name: 'Mens Red Gold Kurta', 
+      description: 'Exclusive Mens fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/w/o/woven-art-silk-sherwani-in-red-v1-myv109.jpg',
+      price:75 }, 
+
+      { name: 'Kids Girls Pink Black Lehanga', 
+      description: 'Exclusive Mens fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/e/m/embroidered-net-lehenga-in-peach-v1-ulc3.jpg',
+      price:75 }, 
+
+      { name: 'Kids Boys Navy Blue Kurta', 
+      description: 'Exclusive kids fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/b/l/block-printed-dupion-silk-kurta-set-in-navy-blue-v1-und598.jpg',
+      price:75 },
+
+      { name: 'Kids Boys Kurta', 
+      description: 'Exclusive kids fashion Utsav Collection',
+      image_url: 'https://medias.utsavfashion.com/media/catalog/product/cache/1/small_image/295x/040ec09b1e35df139433887a97daa66f/w/o/woven-art-silk-jacquard-kurta-set-in-blue-v1-und624.jpg',
+      price:75 },
+
+      { name: 'Red Saree Womens', 
+      description: 'Exclusive Womens fashion Utsav Collection',
+      image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/1/2145sr03-2303.jpg',
+      price:175 },
+
+      { name: 'Blue Green Saree Womens', 
+      description: 'Exclusive Womens fashion Utsav Collection',
+      image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/1/2145sr02-2302.jpg',
+      price:175 },
+
+      { name: 'Indo Western Saree Lehanga', 
+      description: 'Exclusive Womens fashion Panash Collection',
+      image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/1/7/179gw07-sl-707.jpg',
+      price:175 },
+
+      { name: 'Red Gown', 
+      description: 'Exclusive Womens fashion Collection',
+      image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/1/7/178gw01-prn7062.jpg',
+      price:175 },
+
+      { name: 'White Printed Gown', 
+      description: 'Exclusive Womens fashion  Collection',
+      image_url: 'https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/3/9/395kr03-uak100.jpg',
+      price:175 },
+
     { name: 'White Punjabi Suits', price:100 }
 ])
